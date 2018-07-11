9a3599196ffc50bb59b640967157168524685091
paoyuchin <bow1130bow@gmail.com>
2018-07-11 15:30:45 +0800

    1-basic html

---
+++ script/module.js
@@ -0,0 +1,39 @@
+var moment = require('moment');
+console.log(moment)
+
+
+/**
+ * Define module name here 
+ */
+const ModuleName = 'newCalander';
+/**
+ * Props default value write here
+ */
+const ModuleDefaults = {
+    properties: 'value'
+};
+/**
+ * Define you want to get function returns from outside of scope
+ */
+const ModuleReturns = [];
+
+class Module {
+    constructor(ele, options) {
+        this.ele = ele;
+        this.$ele = $(ele);
+        this.option = options;
+    }
+    init() {
+        // first run here
+    }
+    methods() {
+        return this;
+    }
+}
+
+export {
+    ModuleName,
+    ModuleDefaults,
+    ModuleReturns,
+    Module
+};
